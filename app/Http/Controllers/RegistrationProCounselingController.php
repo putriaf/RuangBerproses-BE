9ac635fb4f410a533c813b158a11902e0691f5a3
DESKTOP-FK3FF4R\user <putri.ainur.71@gmail.com>
2022-11-16 08:22:12 +0700

    fix typo

--- app/Http/Controllers/RegistrationProCounselingController.php
+++ app/Http/Controllers/RegistrationProCounselingController.php
@@ -68,7 +68,7 @@ class RegistrationProCounselingController extends Controller
             'pernah_konseling' => 'required',
             'menyakiti_diri' => 'required',
             'mengakhiri_hidup' => 'required',
-            'sesi' => 'requied'
+            'sesi' => 'required'
         ]);
 
         $regprocounseling = RegistrationProCounseling::create($validatedData);
