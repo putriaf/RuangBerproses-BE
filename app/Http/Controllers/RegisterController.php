99b684b06996e7af1664bb192102f44880699e11
DESKTOP-FK3FF4R\user <putri.ainur.71@gmail.com>
2022-12-01 15:40:50 +0700

    modify post req

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -11,20 +11,28 @@ class RegisterController extends Controller
 {
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'nama' => 'required|max:255',
-            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
-            'email' => 'required|email:dns|unique:users',
-            'no_telp' => 'required|numeric|digits_between:10,14',
-            'jk' => 'required|max:1',
-            'tgl_lahir' => 'required',
-            'password' => 'required|min:5|max:255',
-            'role' => 'required',
+        // $validatedData = $request->validate([
+        //     'nama' => 'required|max:255',
+        //     'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+        //     'email' => 'required|email:dns|unique:users',
+        //     'no_telp' => 'required|numeric|digits_between:10,14',
+        //     'jk' => 'required|max:1',
+        //     'tgl_lahir' => 'required',
+        //     'password' => 'required|min:5|max:255',
+        //     'role' => 'required',
+        // ]);
+
+        $register = User::create([
+            'nama' => $request->input('nama'),
+            'username' => $request->input('username'),
+            'email' => $request->input('email'),
+            'no_telp' => $request->input('no_telp'),
+            'jk' => $request->input('jk'),
+            'tgl_lahir' => $request->input('tgl_lahir'),
+            'password' => $request->input('password'),
+            'role' => $request->input('role')
         ]);
 
-        $validatedData['password'] = Hash::make($validatedData['password']);
-
-        $register = User::create($validatedData);
         if ($register) {
             return response()->json([
                 'success' => true,
