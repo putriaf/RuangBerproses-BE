c92a8a9cd2819baa2e71a00c7ec32f76bc4da9df
DESKTOP-FK3FF4R\user <putri.ainur.71@gmail.com>
2022-12-02 23:10:47 +0700

    user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,16 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'nama' => 'Admin - Tim Konseling',
+            'username' => 'konselingrb',
+            'email' => 'konselingrb@mail.com',
+            'password' => Hash::make('admin123'),
+            'role' => '1',
+            'no_telp' => '08718718181',
+            'jk' => 'L',
+            'tgl_lahir' => '27/09/2001'
+        ]);
     }
 }
